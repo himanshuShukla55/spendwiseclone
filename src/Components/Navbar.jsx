7c5fd77ff765c6606c0e38dc476a6fe637d39790
Himanshu Shukla <itachiuchiha@Himanshus-MacBook-Air.local>
2023-10-21 15:25:43 +0530

    feat: Homepage

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,36 +1,65 @@
 import { Link } from "react-router-dom/dist";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../Redux/Reducers/AuthReducer/action";
-import './Navbar.css'
-
+import "./Navbar.css";
 
 export const Navbar = () => {
-   
-  const state=useSelector(store=>store.authState)
-  const dispatch=useDispatch()
- console.log(state)
+  const state = useSelector((store) => store.authState);
+  const dispatch = useDispatch();
+  console.log(state);
 
- const logot=()=>{
-  logout(dispatch)
- }
+  const logot = () => {
+    logout(dispatch);
+  };
 
   return (
     <header>
-    <nav>
-        <ul className='nav-bar'>
-            <li className='logo'><Link style={{textDecoration:'none',color:'white', fontSize:'30px'}} to='/'>SpendWise</Link></li>
-            <input type='checkbox' id='check' />
-            <span className="menu">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/budget">Budget</Link></li>
-                <li>{state.isAuth?<Link onClick={logot}>Logout</Link>:<Link to="/login">Login</Link>}</li>
-                <li>{state.isAuth?<Link >Welcome {state.name}</Link>:<Link to="/signup">SignUp</Link>}</li>
-                
-                <label for="check" className="close-menu"><i className="fas fa-times"></i></label>
-            </span>
-            <label for="check" className="open-menu"><i className="fas fa-bars"></i></label>
+      <nav>
+        <ul className="nav-bar">
+          <li className="logo">
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "white",
+                fontSize: "30px",
+              }}
+              to="/"
+            >
+              SpendWise
+            </Link>
+          </li>
+          <input type="checkbox" id="check" />
+          <span className="menu">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/budget">Budget</Link>
+            </li>
+            <li>
+              {state.isAuth ? (
+                <Link onClick={logot}>Logout</Link>
+              ) : (
+                <Link to="/login">Login</Link>
+              )}
+            </li>
+            <li>
+              {state.isAuth ? (
+                <Link>Welcome {state.name}</Link>
+              ) : (
+                <Link to="/signup">SignUp</Link>
+              )}
+            </li>
+
+            <label htmlFor="check" className="close-menu">
+              <i className="fas fa-times"></i>
+            </label>
+          </span>
+          <label htmlFor="check" className="open-menu">
+            <i className="fas fa-bars"></i>
+          </label>
         </ul>
-    </nav>
+      </nav>
     </header>
   );
-};  
+};
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getBudgets } from "../Redux/Reducers/BudgetReducer/reducer";
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
 
- export const Home=()=>{
-
-    return <h1>Home</h1>
-}
+export const Home = () => {
+  const { isAuth, name } = useSelector((store) => store.authState);
+  const { budgets } = useSelector((store) => store.budgetState);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getBudgets(dispatch);
+  }, []);
+  const totalAmount = budgets.reduce((acc, item) => acc + item.amount, 0);
+  return (
+    <>
+      {isAuth ? (
+        <div id={styles.welcome}>
+          <h3>Hi! {name}</h3>
+          <span>{`Your Budget : ₹${totalAmount}`}</span>
+        </div>
+      ) : (
+        <div id={styles.pleaseLogin}>
+          Please <Link to="/login">Login</Link> to view your wallet details
+        </div>
+      )}
+    </>
+  );
+};
